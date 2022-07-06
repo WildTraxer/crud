b63e94aa1f30f026d655eadd6eff8c2a9c946142
root <root@mx-workstation>
2022-07-06 10:24:46 +0300

    start

--- Insert/index.php
+++ Insert/index.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title> Добавление в таблицу данных телефон</title>
-</head>
-<body>
-    <h1>Добавление данных в таблицу телефон</h1>
-    <form action="insert.php" method="post" name="action">
-        <table>
-            <tr>
-                <td>Введите Номер телефона</td>
-                <td><input type="int" name="Phone"></td>
-            </tr>
-            <tr>
-                <td>Введите ФИО</td>
-                <td><input type="text" name="FIO"></td>
-            </tr>
-            <tr>
-                <td>Введите Дату рождения</td>
-                <td><input type="date" name="DateBirth"></td>
-            </tr>
-            <tr>
-                <td>Введите Регион</td>
-                <td><input type="int" name="Region"></td>
-            </tr>
-            <tr>
-                <td><input type="submit" value="Добавить данные">
-                    <input type="reset" value="Очистить форму">
-                </td>
-            </tr>
 
-        </table>
-    </form>
+<head>
+    <meta charset="utf-8" />
+    <title>Добавление в таблицу данных телефон</title>
+    <link rel="stylesheet" href="../style.css" />
+</head>
+
+<body class="body">
+    <header class="header">
+        <p class="header__logo font-family">CRUD</p>
+    </header>
+    <div class="wrapper wrapper--color">
+        <div class="wrapper__container font-family">
+            <h1 class="h1">Добавление данных в таблицу телефон</h1>
+            <form class="form" action="insert.php" method="post" name="action">
+                <div class="form-item">
+                    <p>Введите Номер телефона</p>
+                    <input class="form-item__input" type="int" name="Phone" />
+                </div>
+                <div class="form-item">
+                    <p>Введите ФИО</p>
+                    <input class="form-item__input" type="text" name="FIO" />
+                </div>
+                <div class="form-item">
+                    <p>Введите Дату рождения</p>
+                    <input class="form-item__input" type="date" name="DateBirth" />
+                </div>
+                <div class="form-item">
+                    <p>Введите Регион</p>
+                    <input class="form-item__input" type="int" name="Region" />
+                </div>
+                <div class="btns">
+                    <input class="btn" type="submit" value="Добавить данные" />
+                    <input class="btn" type="reset" value="Очистить форму" />
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css" />
     <title>Document</title>
 </head>
-<body>
-    <br>
-    <form action = "Insert/index.php" method = "POST">
-        <input type="submit" name = "local" value="Добавить данные">
-    </form> 
-    <br>
-    <form action = "Search/index.php" method = "POST">
-        <input type="submit" name = "local" value="Поиск">
-    </form> 
-    <br>
+
+<body class="body">
+    <header class="header">
+        <p class="header__logo">CRUD</p>
+    </header>
+    <br />
+    <div class="wrapper">
+        <div class="wrapper__container">
+            <div class="btns">
+                <form action="Insert/index.php" method="POST">
+                    <input class="btn" type="submit" name="local" value="Добавить данные" />
+                </form>
+                <br />
+                <form action="Search/index.php" method="POST">
+                    <input class="btn" type="submit" name="local" value="Поиск" />
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <br />
 </body>
+
 </html>
-
-
